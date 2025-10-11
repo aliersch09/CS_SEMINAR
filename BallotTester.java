961a2face62d1457d4623a5ce6b11d59ce0c861f
Amelia Liersch <aliersch@greenwichacademy.org>
2025-10-10 18:16:58 -0700

    still have questions for the tester

---
+++ BallotTester.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class BallotTester{
+    Ballot one = new Ballot();
+    Ballot two = new Ballot("Taylor Swift", "Billie Eilish", "Phoebe Bridgers", "Gracie Abrams");
+    Ballot three = new Ballot("Phoebe Bridgers","Taylor Swift","Gracie Abrams","Billie Eilish");
+    Ballot four = new Ballot("Billie Eilish", "Phoebe Bridgers", "Taylor Swift","Gracie Abrams");
+    Ballot five = new Ballot("Gracie Abrams","Taylor Swift","Phoebe Bridgers","Billie Eilish");
+    ArrayList<Ballot> list = new ArrayList<>(Arrays.asList(one,two,three,four,five));
+    Tally test = new Tally(list);
+    test.countNext();
+    System.out.println(test);
+}

b634cfef84a571d7f56a7ce86e5512995dbcd5f4
Amelia Liersch <aliersch@greenwichacademy.org>
2025-10-16 19:45:35 -0400

    tally class with edits

--- Tally.java
+++ Tally.java
@@ -4,16 +4,19 @@ public class Tally{
     private int[] counter;
     private String[] eliminated;
     private String winner;
-    private int percent;
+    private double percent;
+    private int rounds;
 
     public Tally(ArrayList<Ballot> ballot){
         this.ballot = ballot;
         counter = new int[4];
         eliminated = new String[4];
+        countNext();
     }
 
     public void countNext(){
         for(int j = 0; j <counter.length; j++){
+            rounds++;
             for(int i = 0; i<ballot.size(); i++){
                 String choice = ballot.get(i).getVoteList()[j];
                 if(choice.equals(ballot.get(i).getCandidateList()[0])&&eliminated[j]==null){
@@ -41,7 +44,7 @@ public class Tally{
         int maxIndex = 0;
         int min = counter[0];
         int minIndex = 0;
-        int sum = 0;
+        double sum = 0.0;
         for(int i = 0; i<counter.length; i++){
             if(counter[i]>max){
                 max = counter[i];
@@ -53,7 +56,7 @@ public class Tally{
             }
             sum+=counter[i];
         }
-        percent = (max/sum)*100;
+        percent = ((double)(max)/(sum))*100;
         if(percent>=50){
             is = true;
             winner = ballot.get(0).getCandidateList()[maxIndex];
@@ -70,7 +73,7 @@ public class Tally{
             return "The votes have not been counted yet.";
         }
         else{
-            return "The winner is "+winner+"! There were "+ballot.size()+"rounds, and the winner was "+percent+"% of the votes.";
+            return "The winner is "+winner+"! There were "+rounds+" rounds, and the winner was "+percent+"% of the votes.";
         }
     }
 }

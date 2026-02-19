0dce51110f5f31c574ef919ee82819e9bb3d88c5
Amelia Liersch <aliersch@greenwichacademy.org>
2026-02-19 16:11:41 -0500

    grocery list class with all methods and comments finished

--- GroceryList.java
+++ GroceryList.java
@@ -21,7 +21,7 @@ public class GroceryList extends Node{
             while(curr.next!=null){
                 curr = curr.next;
             }
-            curr.next = incoming;
+            curr.next = incoming;//adds item to the end of list
         }
     }
     //add method with item and index parameter
@@ -30,17 +30,17 @@ public class GroceryList extends Node{
         if(head==null){
             head = incoming;
         }
-        else if (idx<=size() && idx >=0){
+        else if (idx<size() && idx >=0){//if index is accurate
             if(idx==0){
-                incoming.next=head;
+                incoming.next=head;//sets the first value to incoming and moves all other values down one
                 head = incoming;
             }
             else{
                 Node curr = head;
-                for(int i = 0; i<idx-1; i++){
+                for(int i = 0; i<idx-1; i++){//traverse to the index before the idx
                     curr=curr.next;
                 }
-
+            //sets the item in correct index
             incoming.next = curr.next;
             curr.next = incoming;
             }
@@ -53,7 +53,7 @@ public class GroceryList extends Node{
                 head = head.next;
             }else{
                 Node curr = head;
-                for(int i = 0; i<idx; i++){
+                for(int i = 0; i<idx-1; i++){//traverse to the index before the idx
                     curr = curr.next;
                 }
                 curr.next=curr.next.next;
@@ -73,38 +73,50 @@ public class GroceryList extends Node{
     //makes a map of all the items and their prices
     public Map<String, Double> getGroceryMap() throws FileNotFoundException, NullPointerException, NumberFormatException{
         Map<String, Double> map = new HashMap<>();
-        Node curr = head;
-        while(curr!=null){
-            File f = new File("/Users/aliersch/CS/CS_SEMINAR/grocery-items.txt");
-            Scanner sc = new Scanner(f);
-                while(sc.hasNextLine()){
-                    ArrayList<String> line = new ArrayList<>(Arrays.asList(sc.nextLine().split(" ")));
-                    if(curr.data.equals(line.get(0))){
-                        map.put(curr.data, Double.parseDouble(line.get(1)));
-                    }
-                }
-            curr = curr.next;
+        File f = new File("/Users/aliersch/CS/CS_SEMINAR/grocery-items.txt");
+        Scanner sc = new Scanner(f);
+        while(sc.hasNextLine()){//loop through the grocery items txt file
+            String line = sc.nextLine();
+            String currData = line.substring(1, line.indexOf("\" ")); //separate the grocery item
+            if(indexOf(currData)!=-1){//if the grocery item is in the user's list, add it to the map with the corresponding cost
+                map.put(currData, (Double.parseDouble(line.substring(line.indexOf("\" ")+2))));
+            }
         }
         return map;
     }
+    //index of method - checks if that data is in the grocery list, and returns its index
+    public int indexOf(String data){
+        int idx = 0;
+        Node curr = head;
+        while(curr!=null){
+            if(curr.data.equals(data)){
+                return idx;
+            }
+            idx++;
+            curr=curr.next;
+        }
+        return -1;
+    }
     //returns the total cost of the grocery list
     public double getCost() throws FileNotFoundException, NullPointerException{
+        Map<String, Double> map = getGroceryMap();
         double totalCost = 0;
         Node curr = head;
         while(curr!=null){
-            totalCost += getGroceryMap().get(curr.data);
+            totalCost += map.get(curr.data);
             curr = curr.next;
         }
         return totalCost;
     }
-    //returns the grocery list as a list of items with their prices
+    //returns the grocery list as a list of items with their prices - basically printing the map made in getGroceryMap method
     public String toString(){
-        String toRet = "";
+        String toRet = "";//string to return
         try{
+            Map<String, Double> map = getGroceryMap();
             Node curr = head;
-            while(curr!=null){
-                toRet+= curr;
-                toRet+= getGroceryMap().get(curr.data);
+            while(curr!=null){//adds that value and the corresponding cost to the string
+                toRet+= curr.data + " ";
+                toRet+= map.get(curr.data)+"\n";
                 curr=curr.next;
             }
         }catch(Exception ex){
